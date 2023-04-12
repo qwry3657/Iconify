1442cccfe2fb28002be21a7c324004b0277b9fc4
Sa Sajjad <sqsa93103@gmail.com>
2023-04-12 19:12:06 +0600

    push

--- app/src/main/java/com/drdisagree/iconify/ui/activities/IconShape.java
+++ app/src/main/java/com/drdisagree/iconify/ui/activities/IconShape.java
@@ -65,59 +65,8 @@ public class IconShape extends AppCompatActivity {
         icon_shape_preview_styles.add(new Object[]{R.drawable.icon_shape_tapered_rectangle, R.string.icon_mask_style_tapered_rectangle});
         icon_shape_preview_styles.add(new Object[]{R.drawable.icon_shape_vessel, R.string.icon_mask_style_vessel});
         icon_shape_preview_styles.add(new Object[]{R.drawable.icon_shape_rohie_meow, R.string.icon_mask_style_rice_rohie_meow});
-
-        addItem(icon_shape_preview_styles);
-
+        
         refreshBackground();
-    }
-
-    // Function to add new item in list
-    @SuppressLint("UseCompatLoadingForDrawables")
-    private void addItem(ArrayList<Object[]> pack) {
-        for (int i = 0; i < pack.size(); i++) {
-            View list = LayoutInflater.from(this).inflate(R.layout.view_icon_shape, container, false);
-
-            LinearLayout icon_container = list.findViewById(R.id.mask_shape);
-            icon_container.setBackground(getResources().getDrawable((int) pack.get(i)[0]));
-
-            TextView style_name = list.findViewById(R.id.shape_name);
-            style_name.setText(getResources().getString((int) pack.get(i)[1]));
-
-            int finalI = i;
-            list.setOnClickListener(v -> {
-                if (finalI == 0) {
-                    Prefs.putInt(SELECTED_ICON_SHAPE, finalI);
-                    OverlayUtil.disableOverlay("IconifyComponentSIS.overlay");
-                    Toast.makeText(Iconify.getAppContext(), getResources().getString(R.string.toast_disabled), Toast.LENGTH_SHORT).show();
-
-                    refreshBackground();
-                } else {
-                    if (!Environment.isExternalStorageManager()) {
-                        SystemUtil.getStoragePermission(this);
-                    } else {
-                        AtomicBoolean hasErroredOut = new AtomicBoolean(false);
-
-                        try {
-                            hasErroredOut.set(OnDemandCompiler.buildOverlay("SIS", finalI, FRAMEWORK_PACKAGE));
-                        } catch (IOException e) {
-                            hasErroredOut.set(true);
-                            Log.e("IconShape", e.toString());
-                        }
-
-                        if (!hasErroredOut.get()) {
-                            Prefs.putInt(SELECTED_ICON_SHAPE, finalI);
-                            OverlayUtil.enableOverlay("IconifyComponentSIS.overlay");
-                            Toast.makeText(Iconify.getAppContext(), getResources().getString(R.string.toast_applied), Toast.LENGTH_SHORT).show();
-                        } else
-                            Toast.makeText(Iconify.getAppContext(), getResources().getString(R.string.toast_error), Toast.LENGTH_SHORT).show();
-
-                        refreshBackground();
-                    }
-                }
-            });
-
-            container.addView(list);
-        }
     }
 
     // Function to check for bg drawable changes
